3e517f4135da26c2123f7228a79be52e778f9906
Dakota <Dakota242424@live.com>
2023-04-11 14:25:13 -0700

    Added Minimap and updated team colors.

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,21 +7,36 @@ public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] GameObject unitBasePrefab;
     [SerializeField] GameObject gameOverHandlerPrefab;
+    [SerializeField] Color player1;
+    [SerializeField] Color player2;
+    [SerializeField] Color player3;
+    [SerializeField] Color player4;
 
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
         base.OnServerAddPlayer(conn);
         RTSPlayer rtsPlayer = conn.identity.GetComponent<RTSPlayer>();
-        rtsPlayer.SetTeamColor(new Color(
-            UnityEngine.Random.Range(0f, 1f),
-            UnityEngine.Random.Range(0f, 1f),
-            UnityEngine.Random.Range(0f, 1f)
-            ));
+
+        print(conn.connectionId);
+        Color teamColor;
+
+        teamColor = conn.connectionId switch
+        {
+            0 => player1,
+            1 => player2,
+            2 => player3,
+            3 => player4,
+            _ => new Color(
+                        UnityEngine.Random.Range(0f, 1f),
+                        UnityEngine.Random.Range(0f, 1f),
+                        UnityEngine.Random.Range(0f, 1f)),
+        };
+
+        rtsPlayer.SetTeamColor(teamColor);
 
         GameObject unitSpawner = Instantiate(unitBasePrefab, conn.identity.transform.position, conn.identity.transform.rotation);
         NetworkServer.Spawn(unitSpawner, conn);
-
     }
 
     public override void OnServerSceneChanged(string sceneName)
